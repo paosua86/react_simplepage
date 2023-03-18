3b3d4cc8882fb89eae3f34b23e1d31a59788379c
paosua86 <paolasuarezb1986@gmail.com>
2023-03-18 15:45:14 -0500

    links and pages complete

--- src/components/individual/individual.jsx
+++ src/components/individual/individual.jsx
@@ -15,10 +15,25 @@ class Individual extends React.Component {
   render() {
     return (
       <section className="individual" id="individual">
+        <img data-aos="fade-right" className="bike-img-estatico" src="images/bike.svg" alt="" />
         <h2 data-aos="fade-right" className="heading">SEGUROS INDIVIDUALES</h2>
         <p class="description-service" data-aos="fade-left">No podemos garantizar tu vida ... pero SÍ la tranquilidad económica de quienes amas</p>
+
+        <div class="opcionesindividual">
+            <div class="atencionMedica-box">
+              <a href="/atencionmedica"><p>Seguro de vida</p></a>
+            </div>
+            <div class="atencionMedica-box">
+              <a href="/atencionmedica/"><p>Gastos Médicos Mayores</p></a>
+            </div>
+            <div class="atencionMedica-box">
+              <a href="/viajes"><p>Seguro de viajes</p></a>
+            </div>
+          </div>
+
+
         <div className="individual-container">
-          <div  className="individual-box">
+          <div className="individual-box">
             <i className='bx bx-check-circle'></i>
             <p>Seguros de Vida de hasta de USD 1.000.000</p>
           </div>
@@ -30,37 +45,24 @@ class Individual extends React.Component {
             <i className='bx bx-check-circle'></i>
             <p>Planes de Jubilación.</p>
           </div>
-          <div  className="individual-box">
+          <div className="individual-box">
             <i className='bx bx-check-circle'></i>
             <p>Seguros de Asistencia Médica</p>
           </div>
-          <div  className="individual-box">
+          <div className="individual-box">
             <i className='bx bx-check-circle'></i>
             <p>Accidentes Personales</p>
           </div>
-          <div  className="individual-box">
+          <div className="individual-box">
             <i className='bx bx-check-circle'></i>
             <p>Cobertura de mascotas</p>
           </div>
-          <div  className="individual-box">
+          <div className="individual-box">
             <i className='bx bx-check-circle'></i>
             <p>Seguro de viaje</p>
           </div>
 
-          <div class="opcionesindividual">
-          <div class="atencionMedica-box">
-            <i className="bx bx-check-circle"></i>
-            <a href="/atencionmedica"><p>Seguro de vida</p></a>
-          </div>
-          <div class="atencionMedica-box">
-            <i className="bx bx-check-circle"></i>
-            <a href="/atencionmedica/"><p>Gastos Médicos Mayores</p></a>
-          </div>
-          <div class="atencionMedica-box">
-            <i className="bx bx-check-circle"></i>
-            <a href="/viajes"><p>Seguro de viajes</p></a>
-          </div>
-          </div>
+
         </div>
       </section>
     );
